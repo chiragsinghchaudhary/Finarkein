f1faa5121c4c2f8752854bd561d3c1f4aabad1d9
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:28:03 +0530

    Update DepositTransactionDTO.java

--- src/main/java/com/ashika/model/dto/DepositTransactionDTO.java
+++ src/main/java/com/ashika/model/dto/DepositTransactionDTO.java
@@ -1,1 +1,17 @@
+package com.ashika.model.dto;
+
+public record DepositTransactionDTO(
+        Long id,
+        String endDate,
+        String startDate,
+        Double amount,
+        Double currentBalance,
+        String mode,
+        String narration,
+        String reference,
+        String transactionId,
+        Long transactionTimestamp,
+        String type,
+        String valueDate
+) {}
 
