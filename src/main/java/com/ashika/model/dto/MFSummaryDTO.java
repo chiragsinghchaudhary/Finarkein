edf7f2701ad520e9f1bc57317919e28b654da560
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:51:28 +0530

    Update MFSummaryDTO.java

--- src/main/java/com/ashika/model/dto/MFSummaryDTO.java
+++ src/main/java/com/ashika/model/dto/MFSummaryDTO.java
@@ -1,1 +1,24 @@
+package com.ashika.model.dto;
 
+public record MFSummaryDTO(
+        Long id,
+        Double costValue,
+        Double currentValue,
+        String fatcaStatus,
+        String amc,
+        String amfiCode,
+        Double closingUnits,
+        String folioNo,
+        String isin,
+        String isinDescription,
+        Double lienUnits,
+        Double lockinUnits,
+        Double nav,
+        String navDate,
+        String registrar,
+        String schemeCategory,
+        String schemeCode,
+        String schemeOption,
+        String schemeTypes,
+        String ucc
+) {}
