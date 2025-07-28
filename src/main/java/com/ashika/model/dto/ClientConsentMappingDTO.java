72f0d9fe59fef0b16b593aa3d53d3c80b9ca6002
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:58:01 +0530

    Update ClientConsentMappingDTO.java

--- src/main/java/com/ashika/model/dto/ClientConsentMappingDTO.java
+++ src/main/java/com/ashika/model/dto/ClientConsentMappingDTO.java
@@ -1,1 +1,15 @@
+package com.ashika.model.dto;
 
+import java.time.LocalDate;
+
+public record ClientConsentMappingDTO(
+        String clientCode,
+        String pan,
+        String state,
+        String consentStatus,
+        String dataFetchStatus,
+        LocalDate dob,
+        String email,
+        String requestId,
+        String consentHandle
+) {}
