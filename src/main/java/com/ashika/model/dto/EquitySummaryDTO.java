bbf9b8a5c4ad005f452b81c045f783e3fdc444ee
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:31:25 +0530

    Update EquitySummaryDTO.java

--- src/main/java/com/ashika/model/dto/EquitySummaryDTO.java
+++ src/main/java/com/ashika/model/dto/EquitySummaryDTO.java
@@ -1,1 +1,13 @@
+package com.ashika.model.dto;
+
+public record EquitySummaryDTO(
+        Long id,
+        Double currentValue,
+        String holdingMode,
+        String isin,
+        String isinDescription,
+        String issuerName,
+        Double lastTradedPrice,
+        Double units
+) {}
 
