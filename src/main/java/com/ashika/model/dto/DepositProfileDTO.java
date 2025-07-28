5e67cc1c63026052a29ebeaf280c5d184e65aa43
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:13:22 +0530

    Update DepositProfileDTO.java

--- src/main/java/com/ashika/model/dto/DepositProfileDTO.java
+++ src/main/java/com/ashika/model/dto/DepositProfileDTO.java
@@ -1,1 +1,16 @@
+package com.ashika.model.dto;
+
+public record DepositProfileDTO(
+    Long id,
+    String type,
+    String address,
+    Boolean ckycCompliance,
+    Long dob,
+    String email,
+    String landline,
+    String mobile,
+    String name,
+    String nominee,
+    String pan
+) {}
 
