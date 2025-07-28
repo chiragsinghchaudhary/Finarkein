e502490334590966ba01c80be0734854c12f2933
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:55:10 +0530

    Update MFTransactionDTO.java

--- src/main/java/com/ashika/model/dto/MFTransactionDTO.java
+++ src/main/java/com/ashika/model/dto/MFTransactionDTO.java
@@ -1,1 +1,27 @@
+package com.ashika.model.dto;
+
+public record MFTransactionDTO(
+        Long id,
+        String startDate,
+        String endDate,
+        String amc,
+        String amfiCode,
+        Double amount,
+        String isin,
+        String isinDescription,
+        Integer lockInDays,
+        String lockInFlag,
+        String mode,
+        String narration,
+        Double nav,
+        String navDate,
+        String registrar,
+        String schemeCode,
+        String schemePlan,
+        String transactionDate,
+        String txnId,
+        String type,
+        String ucc,
+        Double units
+) {}
 
