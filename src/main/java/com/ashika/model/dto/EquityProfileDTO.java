9349dde9fac0301d90f219a8e230d1911cce4820
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:30:18 +0530

    Update EquityProfileDTO.java

--- src/main/java/com/ashika/model/dto/EquityProfileDTO.java
+++ src/main/java/com/ashika/model/dto/EquityProfileDTO.java
@@ -1,1 +1,16 @@
+package com.ashika.model.dto;
+
+public record EquityProfileDTO(
+        Long id,
+        String address,
+        String dematId,
+        Long dob,
+        String email,
+        Boolean kycCompliance,
+        String landline,
+        String mobile,
+        String name,
+        String nominee,
+        String pan
+) {}
 
