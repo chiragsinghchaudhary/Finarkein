c2cdfba94ca7999517695604310b89fa35f0591d
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-28 16:30:01 +0530

    Update EquityProfileEntity.java

--- src/main/java/com/ashika/model/entity/EquityProfileEntity.java
+++ src/main/java/com/ashika/model/entity/EquityProfileEntity.java
@@ -1,1 +1,117 @@
+package com.ashika.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "equity_profile")
+public class EquityProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+    private String dematId;
+    private Long dob;
+    private String email;
+    private Boolean kycCompliance;
+    private String landline;
+    private String mobile;
+    private String name;
+    private String nominee;
+    private String pan;
+
+    // ===== Getters & Setters =====
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDematId() {
+        return dematId;
+    }
+
+    public void setDematId(String dematId) {
+        this.dematId = dematId;
+    }
+
+    public Long getDob() {
+        return dob;
+    }
+
+    public void setDob(Long dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getKycCompliance() {
+        return kycCompliance;
+    }
+
+    public void setKycCompliance(Boolean kycCompliance) {
+        this.kycCompliance = kycCompliance;
+    }
+
+    public String getLandline() {
+        return landline;
+    }
+
+    public void setLandline(String landline) {
+        this.landline = landline;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNominee() {
+        return nominee;
+    }
+
+    public void setNominee(String nominee) {
+        this.nominee = nominee;
+    }
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+}
 
