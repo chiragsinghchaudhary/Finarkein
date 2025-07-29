837a447e26e6012ea7754e3c37bf5ad379960431
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-29 11:21:26 +0530

    Update MyService.java

--- src/main/java/com/ashikha/service/MyService.java
+++ src/main/java/com/ashikha/service/MyService.java
@@ -84,4 +84,3 @@ public interface MyService {
     ClientConsentMappingHistDTO updateClientConsentHistory(Long id, ClientConsentMappingHistDTO dto);
     void deleteClientConsentHistory(Long id);
 }
-
