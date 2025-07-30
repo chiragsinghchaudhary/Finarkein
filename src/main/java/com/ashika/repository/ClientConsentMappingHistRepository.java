a1e03bb9ddd0eee22885b1a0c58b75c5b398bec3
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-30 12:49:26 +0530

    Update ClientConsentMappingHistRepository.java

--- src/main/java/com/ashika/repository/ClientConsentMappingHistRepository.java
+++ src/main/java/com/ashika/repository/ClientConsentMappingHistRepository.java
@@ -1,6 +1,5 @@
 package com.ashika.repository;
 
-
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
@@ -15,188 +14,238 @@ import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuer
 import com.ashika.model.entity.ClientConsentMappingHistEntity;
 import com.ashika.model.entity.ClientConsentMappingHistId;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
 public class ClientConsentMappingHistRepository implements JpaRepository<ClientConsentMappingHistEntity, ClientConsentMappingHistId> {
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> List<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    // Inject EntityManager for custom queries
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Override
-	public List<ClientConsentMappingHistEntity> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> List<S> saveAll(Iterable<S> entities) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public List<ClientConsentMappingHistEntity> findAllById(Iterable<ClientConsentMappingHistId> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<ClientConsentMappingHistEntity> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<ClientConsentMappingHistEntity> findAllById(Iterable<ClientConsentMappingHistId> ids) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Optional<ClientConsentMappingHistEntity> findById(ClientConsentMappingHistId id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> S save(S entity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public boolean existsById(ClientConsentMappingHistId id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public Optional<ClientConsentMappingHistEntity> findById(ClientConsentMappingHistId id) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
 
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean existsById(ClientConsentMappingHistId id) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void deleteById(ClientConsentMappingHistId id) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public long count() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void delete(ClientConsentMappingHistEntity entity) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void deleteById(ClientConsentMappingHistId id) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void deleteAllById(Iterable<? extends ClientConsentMappingHistId> ids) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void delete(ClientConsentMappingHistEntity entity) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void deleteAll(Iterable<? extends ClientConsentMappingHistEntity> entities) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void deleteAllById(Iterable<? extends ClientConsentMappingHistId> ids) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void deleteAll(Iterable<? extends ClientConsentMappingHistEntity> entities) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public List<ClientConsentMappingHistEntity> findAll(Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public Page<ClientConsentMappingHistEntity> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<ClientConsentMappingHistEntity> findAll(Sort sort) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> Optional<S> findOne(Example<S> example) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
-	}
+    @Override
+    public Page<ClientConsentMappingHistEntity> findAll(Pageable pageable) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> Page<S> findAll(Example<S> example, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> Optional<S> findOne(Example<S> example) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> long count(Example<S> example) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> Page<S> findAll(Example<S> example, Pageable pageable) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> boolean exists(Example<S> example) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> long count(Example<S> example) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity, R> R findBy(Example<S> example,
-			Function<FetchableFluentQuery<S>, R> queryFunction) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> boolean exists(Example<S> example) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void flush() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity, R> R findBy(Example<S> example,
+            Function<FetchableFluentQuery<S>, R> queryFunction) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> S saveAndFlush(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void flush() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> List<S> saveAllAndFlush(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> S saveAndFlush(S entity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void deleteAllInBatch(Iterable<ClientConsentMappingHistEntity> entities) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> List<S> saveAllAndFlush(Iterable<S> entities) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void deleteAllByIdInBatch(Iterable<ClientConsentMappingHistId> ids) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void deleteAllInBatch(Iterable<ClientConsentMappingHistEntity> entities) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public void deleteAllInBatch() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void deleteAllByIdInBatch(Iterable<ClientConsentMappingHistId> ids) {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public ClientConsentMappingHistEntity getOne(ClientConsentMappingHistId id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void deleteAllInBatch() {
+        // TODO Auto-generated method stub
+        
+    }
 
-	@Override
-	public ClientConsentMappingHistEntity getById(ClientConsentMappingHistId id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ClientConsentMappingHistEntity getOne(ClientConsentMappingHistId id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public ClientConsentMappingHistEntity getReferenceById(ClientConsentMappingHistId id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ClientConsentMappingHistEntity getById(ClientConsentMappingHistId id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> List<S> findAll(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ClientConsentMappingHistEntity getReferenceById(ClientConsentMappingHistId id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <S extends ClientConsentMappingHistEntity> List<S> findAll(Example<S> example, Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <S extends ClientConsentMappingHistEntity> List<S> findAll(Example<S> example) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-		
+    @Override
+    public <S extends ClientConsentMappingHistEntity> List<S> findAll(Example<S> example, Sort sort) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    // ----------------------
+    // CUSTOM QUERY METHODS
+    // ----------------------
+
+    // 1. Check valid consent (runType = consent, state = SUCCESS)
+    public List<ClientConsentMappingHistEntity> checkValidConsent() {
+        String jpql = "SELECT c FROM ClientConsentMappingHistEntity c " +
+                      "WHERE c.runType = 'consent' AND c.state = 'SUCCESS'";
+        TypedQuery<ClientConsentMappingHistEntity> query = entityManager.createQuery(jpql, ClientConsentMappingHistEntity.class);
+        return query.getResultList();
+    }
+
+    // 2. Get all DB records
+    public List<ClientConsentMappingHistEntity> getDbRecords() {
+        String jpql = "SELECT c FROM ClientConsentMappingHistEntity c";
+        TypedQuery<ClientConsentMappingHistEntity> query = entityManager.createQuery(jpql, ClientConsentMappingHistEntity.class);
+        return query.getResultList();
+    }
+
+    // 3. Get result request by resultRequest field
+    public List<ClientConsentMappingHistEntity> getResultRequest(String resultRequest) {
+        String jpql = "SELECT c FROM ClientConsentMappingHistEntity c WHERE c.resultRequest = :resultRequest";
+        TypedQuery<ClientConsentMappingHistEntity> query = entityManager.createQuery(jpql, ClientConsentMappingHistEntity.class);
+        query.setParameter("resultRequest", resultRequest);
+        return query.getResultList();
+    }
+
+    // 4. Get status by finarchinHitResponse
+    public List<ClientConsentMappingHistEntity> getStatus(String response) {
+        String jpql = "SELECT c FROM ClientConsentMappingHistEntity c WHERE c.finarchinHitResponse = :response";
+        TypedQuery<ClientConsentMappingHistEntity> query = entityManager.createQuery(jpql, ClientConsentMappingHistEntity.class);
+        query.setParameter("response", response);
+        return query.getResultList();
+    }
+
+    // 5. Get result combining valid consent + resultRequest
+    public List<ClientConsentMappingHistEntity> getResult(String resultRequest) {
+        String jpql = "SELECT c FROM ClientConsentMappingHistEntity c " +
+                      "WHERE c.state = 'SUCCESS' AND c.runType = 'consent' AND c.resultRequest = :resultRequest";
+        TypedQuery<ClientConsentMappingHistEntity> query = entityManager.createQuery(jpql, ClientConsentMappingHistEntity.class);
+        query.setParameter("resultRequest", resultRequest);
+        return query.getResultList();
+    }
 }
