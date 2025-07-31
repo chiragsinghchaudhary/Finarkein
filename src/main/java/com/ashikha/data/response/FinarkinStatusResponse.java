187e226b9407597610d1660d9a352a090c0182b1
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-07-31 12:31:27 +0530

    Update FinarkinStatusResponse.java

--- src/main/java/com/ashikha/data/response/FinarkinStatusResponse.java
+++ src/main/java/com/ashikha/data/response/FinarkinStatusResponse.java
@@ -1,1 +1,41 @@
+package com.ashikha.data.response;
 
+public class FinarkinStatusResponse {
+    private String requestId;
+    private String state;
+    private String consentStatus;
+    private String dataFetchStatus;
+
+    // Getters and setters
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getConsentStatus() {
+        return consentStatus;
+    }
+
+    public void setConsentStatus(String consentStatus) {
+        this.consentStatus = consentStatus;
+    }
+
+    public String getDataFetchStatus() {
+        return dataFetchStatus;
+    }
+
+    public void setDataFetchStatus(String dataFetchStatus) {
+        this.dataFetchStatus = dataFetchStatus;
+    }
+}
