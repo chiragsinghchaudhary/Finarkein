56d5fc91247dd9d76dd17d415543ed8dee72984b
chiragkapasi-ashika <chiragkapasi@ashikagroup.com>
2025-08-01 16:04:37 +0530

    fix

--- src/main/java/com/ashika/service/MyService.java
+++ src/main/java/com/ashika/service/MyService.java
@@ -2,9 +2,6 @@ package com.ashika.service;
 
 import com.ashikha.data.request.ConsentNewRunRequest;
 import com.ashikha.data.request.GetRequest;
-import com.ashikha.data.request.GetResultRequest;
-import com.ashikha.data.request.GetStatusRequest;
-import com.ashikha.data.request.RecurringNewRunRequest;
 import com.ashikha.data.response.ConsentNewRunResponse;
 import com.ashikha.data.response.GetResultResponse;
 import com.ashikha.data.response.GetStatusResponse;
